09abea2e624bbc493b25127f22a59832a5ac1cba
Eric <mugoericpg20@gmail.com>
2023-03-28 14:53:25 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
+   return fetch('https://anapioficeandfire.com/api/books')
+      .then(response => response.json())
+      .then(json => renderBooks(json));
+
+
+
+
   // To pass the tests, don't forget to return your fetch!
-  
+
 }
 
 function renderBooks(books) {
